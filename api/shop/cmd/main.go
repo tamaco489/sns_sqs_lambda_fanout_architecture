f660702b271455c591103ebd0b3be089cca8f5cf
tamaco489 <tamaco489@gmail.com>
2025-06-01 09:43:39 +0900

    setup project

---
+++ api/shop/cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server starting on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ api/shop/go.mod
@@ -0,0 +1,3 @@
+module github.com/tamaco489/sns_sqs_lambda_fanout_architecture/api/shop
+
+go 1.23.9
