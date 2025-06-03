20643d5a316a5f90be942768b463a1b840916263
tamaco489 <tamaco489@gmail.com>
2025-06-04 00:42:13 +0900

    feature/slack message ecr (#8)
    * add: line message service directory
    
    * fix: slack message ecr repository

--- infra/ecr/output.tf
+++ infra/ecr/output.tf
@@ -6,3 +6,12 @@ output "shop_api" {
     url  = aws_ecr_repository.shop_api.repository_url
   }
 }
+
+output "slack_message_batch" {
+  value = {
+    arn  = aws_ecr_repository.slack_message_batch.arn
+    id   = aws_ecr_repository.slack_message_batch.id
+    name = aws_ecr_repository.slack_message_batch.name
+    url  = aws_ecr_repository.slack_message_batch.repository_url
+  }
+}
