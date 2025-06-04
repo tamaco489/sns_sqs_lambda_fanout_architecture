9c976bf227256669c061da8fa34c259d1185f7b2
tamaco489 <tamaco489@gmail.com>
2025-06-04 21:27:53 +0900

    fix: sqs.tf fmt

--- infra/sqs/sqs.tf
+++ infra/sqs/sqs.tf
@@ -3,9 +3,9 @@ resource "aws_sqs_queue" "slack_message" {
   fifo_queue                  = false
   content_based_deduplication = false
   delay_seconds               = 0
-  max_message_size            = 256 * 1024  # 256KB
-  message_retention_seconds   = 1 * 24 * 60 * 60  # 1日間
-  visibility_timeout_seconds  = 15 # NOTE: 本番運用時はもう少し長めにしても良い
+  max_message_size            = 256 * 1024       # 256KB
+  message_retention_seconds   = 1 * 24 * 60 * 60 # 1日間
+  visibility_timeout_seconds  = 15               # NOTE: 本番運用時はもう少し長めにしても良い
   receive_wait_time_seconds   = 0
 
   tags = { Name = "slack-message-sqs" }
@@ -16,9 +16,9 @@ resource "aws_sqs_queue" "line_message" {
   fifo_queue                  = false
   content_based_deduplication = false
   delay_seconds               = 0
-  max_message_size            = 256 * 1024  # 256KB
-  message_retention_seconds   = 1 * 24 * 60 * 60  # 1日間
-  visibility_timeout_seconds  = 15 # NOTE: 本番運用時はもう少し長めにしても良い
+  max_message_size            = 256 * 1024       # 256KB
+  message_retention_seconds   = 1 * 24 * 60 * 60 # 1日間
+  visibility_timeout_seconds  = 15               # NOTE: 本番運用時はもう少し長めにしても良い
   receive_wait_time_seconds   = 0
 
   tags = { Name = "line-message-sqs" }
