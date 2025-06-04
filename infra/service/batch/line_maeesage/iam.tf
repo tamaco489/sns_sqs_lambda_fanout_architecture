bae5b70b67a1d21b0f0878160c95f68f0ef20b4c
tamaco489 <tamaco489@gmail.com>
2025-06-05 02:41:49 +0900

    add: send line message batch vpc lambda service resource

---
+++ infra/service/batch/line_maeesage/iam.tf
@@ -0,0 +1,64 @@
+# =================================================================
+# basic iam policy
+# =================================================================
+data "aws_iam_policy_document" "lambda_execution_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "line_message_batch" {
+  name               = "${local.fqn}-batch-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_execution_assume_role.json
+  tags               = { Name = "${local.fqn}-batch" }
+}
+
+# https://docs.aws.amazon.com/ja_jp/aws-managed-policy/latest/reference/AWSLambdaVPCAccessExecutionRole.html
+resource "aws_iam_role_policy_attachment" "line_message_batch_execution_role" {
+  role       = aws_iam_role.line_message_batch.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+# =================================================================
+# sqs iam policy
+# =================================================================
+data "aws_iam_policy_document" "line_message_batch_sqs_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = [
+      data.terraform_remote_state.sqs.outputs.line_message_lambda_process_standard_sqs.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:SendMessage"
+    ]
+    resources = [
+      data.terraform_remote_state.sqs.outputs.line_message_lambda_process_dlq.arn
+    ]
+  }
+}
+
+# SQSへのアクセス権を定義するポリシードキュメントをIAM Policyとして定義し、IAM Roleに関連付ける
+resource "aws_iam_policy" "line_message_batch_sqs_policy" {
+  name        = "${local.fqn}-batch-sqs-policy"
+  description = "policy to allow lambdas to retrieve queued data"
+  policy      = data.aws_iam_policy_document.line_message_batch_sqs_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "line_message_batch_sqs_role" {
+  role       = aws_iam_role.line_message_batch.name
+  policy_arn = aws_iam_policy.line_message_batch_sqs_policy.arn
+}
---
+++ infra/service/batch/line_maeesage/variables.tf
@@ -0,0 +1,30 @@
+# =================================================================
+# general
+# =================================================================
+variable "env" {
+  description = "The environment in which the line message vpc lambda will be created"
+  type        = string
+  default     = "stg"
+}
+
+variable "project" {
+  description = "The project name"
+  type        = string
+  default     = "fanout-architecture"
+}
+
+variable "region" {
+  description = "The region in which the line message vpc lambda will be created"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "product" {
+  description = "The product name"
+  type        = string
+  default     = "line-message"
+}
+
+locals {
+  fqn = "${var.env}-${var.product}"
+}
