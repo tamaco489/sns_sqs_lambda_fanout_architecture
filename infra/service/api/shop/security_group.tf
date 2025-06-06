a41e8dcfdcd9ccfacb3b42b4b5e52b000c8f53b2
tamaco489 <tamaco489@gmail.com>
2025-06-06 21:54:32 +0900

    add: sns vpc endpoint securit group

--- infra/service/api/shop/security_group.tf
+++ infra/service/api/shop/security_group.tf
@@ -6,9 +6,31 @@ resource "aws_security_group" "shop_api" {
   tags = { Name = "${local.fqn}-api" }
 }
 
-resource "aws_vpc_security_group_egress_rule" "shop_api_egress" {
-  security_group_id = aws_security_group.shop_api.id
-  description       = "Allow Lambda to access external resources (e.g. RDS, DynamoDB, APIs)"
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
+# SNS VPCエンドポイント用セキュリティグループ
+resource "aws_security_group" "sns_endpoint" {
+  name        = "${local.fqn}-sns-endpoint"
+  description = "Security group for SNS VPC Endpoint"
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc.id
+
+  tags = { Name = "${local.fqn}-sns-endpoint" }
+}
+
+# shop_api セキュリティグループ → sns_endpoint セキュリティグループ への HTTPS 通信許可（egress）
+resource "aws_vpc_security_group_egress_rule" "shop_api_to_sns_endpoint" {
+  security_group_id            = aws_security_group.shop_api.id
+  description                  = "Allow HTTPS to SNS VPC endpoint"
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.sns_endpoint.id
+}
+
+# sns_endpoint セキュリティグループ ← shop_api  セキュリティグループ からの HTTPS 通信許可（ingress）
+resource "aws_vpc_security_group_ingress_rule" "sns_endpoint_from_shop_api" {
+  security_group_id            = aws_security_group.sns_endpoint.id
+  description                  = "Allow HTTPS from API"
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.shop_api.id
 }
